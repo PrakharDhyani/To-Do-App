e05eba651870cab948e63ee3365e0b184eac5827
Prakhar Dhyani <prakhardhyani1000@gmail.com>
2022-02-11 23:14:09 +0530

    initializing firebase

---
+++ todo_app/src/firebase.js
@@ -0,0 +1,17 @@
+import {intializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDF8S-No_ZC0RBiCDpqElCPuFPfUGWRNWg",
+    authDomain: "to-do-app-9f192.firebaseapp.com",
+    projectId: "to-do-app-9f192",
+    storageBucket: "to-do-app-9f192.appspot.com",
+    messagingSenderId: "367818556267",
+    appId: "1:367818556267:web:db29d1847f398724d1c22e"
+  };
+
+  const app = intializeApp(firebaseConfig);
+  const db = getFirestore(firebaseConfig);
+
+  export {db};
