5245369043fb236b96b52106e0cbaa52c4cb0bf7
Prakhar Dhyani <prakhardhyani1000@gmail.com>
2022-02-12 23:08:21 +0530

    AddToDo

---
+++ todo_app/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title(props) {
+    return (
+        <div className='title'>
+            <h1>To do app</h1>            
+        </div>
+    );
+}
+
+export default Title;
